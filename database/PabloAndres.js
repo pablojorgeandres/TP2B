847b5b8826be62478fe526ec136c206bdca19db7
Pablo Andres (Acceleration) <Pablo.Andres@Acceleration.biz>
2020-05-11 15:41:01 -0300

    Desafío 2 corregido.

--- database/PabloAndres.js
+++ database/PabloAndres.js
@@ -1,89 +1,67 @@
-const mongoClient = require('mongodb').MongoClient;
-const uriDatabase = "mongodb+srv://pablo:ClusterPass2020@cluster-000-mjcet.azure.mongodb.net/test?retryWrites=true&w=majority";
-const chalk = require('chalk');
-
-const client = new mongoClient(uriDatabase, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const ins = "insert"
-const updt = "update"
-const del = "delete";
-
 const inventor = {
     first: "Nikola",
     last: "Tesla",
     year: 1856
 }
-const inventor2 = {
-    first: "Alan",
-    last: "Turing",
-    year: 1912
-}
-const updateData = { $set: { "year": 1910 } }
-const updateID = { "first": "Alan" };
+const updateData = { $set: { "year": 2020 } }
+const filter = {first: "Nikola"}
 
-const deleteData = {
-    "first": "Alan",
-}
+const mongoClient = require('mongodb').MongoClient;
+const uriDatabase = "mongodb+srv://pablo:ClusterPass2020@cluster-000-mjcet.azure.mongodb.net/test?retryWrites=true&w=majority";
 
-async function connectWithDB(op, object, filter) {
-    return new Promise((resolve, reject) => {
-        client.connect((error, result) => {
-            if (!error) {
-                console.log(chalk.green("Conexión exitosa"));
-                const collectionInventors = result.db('sample_betp2').collection('inventors');
-                switch (op) {
-                    case "insert":
-                        collectionInventors.insertOne(object, (error, result) => {
-                            if (!error) {
-                                console.log(chalk.green("Iventor insertado correctamente", result));
-                            } else {
-                                console.log(chalk.red("Falla, iventor no insertado.", error));
-                            }
-                        });
-                        break;
-                    case "update":
-                        collectionInventors.updateOne(filter, object, (error, result) => {
-                            if (!error) {
-                                console.log(chalk.green("Iventor updateado correctamente", result));
-                            } else {
-                                console.log(chalk.red("Falla, iventor no updateado.", error));
-                            }
-                        });
-                        break;
-                    case "delete":
-                        collectionInventors.deleteOne(object, (error, result) => {
-                            if (!error) {
-                                console.log(chalk.green("Iventor eliminado correctamente", result));
-                            } else {
-                                console.log(chalk.red("Falla, iventor no eliminado.", error));
-                            }
-                        });
-                        break;
-                    default:
-                        break;
-                }
+const chalk = require('chalk');
 
-            } else {
-                console.log(chalk.red(error));
-            }
-        });
-    })
+const client = new mongoClient(uriDatabase, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect((error, result) => {
+    if (!error) {
+        console.log(chalk.green("Conexión exitosa"));
+        const collectionInventors = result.db('sample_betp2').collection('inventors');
+
+        (async function(){
+          await insertInventor(collectionInventors, inventor).then(message => console.log(message))
+          await updInventor(collectionInventors, filter, updateData).then(message => console.log(message))
+          await deleteInventor(collectionInventors, filter).then(message => console.log(message))
+          client.close()
+        })()
+
+    } else {
+        console.log(chalk.red(error));
+    }
+});
+
+function insertInventor(invConnect, inventor) {
+  return new Promise((resolve, reject) => {
+    invConnect.insertOne(inventor, (error, result) => {
+      if (!error) {
+        resolve(chalk.green("Iventor insertado correctamente", result));
+      } else {
+        reject(chalk.red("Falla, iventor no insertado.", error));
+      }
+    });
+  })
 }
 
-async function executeAsyncToDB(op, object, filter) {
-    console.log('llamando ...')
-    const resultado = await connectWithDB(op, object, filter)
-    console.log(resultado)
-    client.close()
+function updInventor(invConnect, filter, updateInventor) {
+  return new Promise((resolve, reject) => {
+    invConnect.updateOne(filter, updateInventor, (error, result) => {
+      if (!error) {
+        resolve(chalk.green("Iventor insertado correctamente", result));
+      } else {
+        reject(chalk.red("Falla, iventor no updateado.", error));
+      }
+    });
+  })
 }
 
-/**** Descomentar la operación que desea ejecutar ****/
-
-/* Insert */
-executeAsyncToDB(ins, inventor2, null);
-
-/* Update */
-// executeAsyncToDB(updt, updateData, updateID);
-
-/* Delete */
-// executeAsyncToDB(del, deleteData, null);
+function deleteInventor(invConnect, inventor) {
+  return new Promise((resolve, reject) => {
+    invConnect.deleteOne(inventor, (error, result) => {
+        if (!error) {
+            resolve (chalk.green("Iventor eliminado correctamente", result));
+        } else {
+            reject (chalk.red("Falla, iventor no eliminado.", error));
+        }
+    });
+  })
+}
